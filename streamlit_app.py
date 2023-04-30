8ce821731db060ed88669217b8d1f823b31b73d6
Prathamesh Kodilkar <100465080+PrathameshKodilkar@users.noreply.github.com>
2023-04-30 21:51:00 +0530

    Update streamlit_app.py
    changed header
    add one item to favorites
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.header ('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Bioled Free-Range egg')
+streamlit.header ('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-Bioled Free-Range egg')
+streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
